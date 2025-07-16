1065ea10da3e29728fb5ae3716072dd26a578e77
Romdik <romdik83@gmail.com>
2025-07-16 19:04:04 +0300

    Platform

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -1,13 +1,8 @@
 package ru.netology.unit;
 
-//import org.junit.jupiter.api.Test;
-//import static org.junit.jupiter.api.Assertions.*;
-//import org.testng.annotations.Test;
-//import static org.testng.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
 
@@ -30,15 +25,6 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    public void whenBuyingFor1000TheSystemDoesNotOfferAnything() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 1000;
-        int actual= service.remain(amount);
-        int expected=0;
-        assertEquals(expected, actual);
-    }
-
-    @Test
     public void whenBuyingAt1900TheSystemOffersToBuyAnother100() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1900;
@@ -54,5 +40,5 @@ public class CashbackHackServiceTest {
         int actual= service.remain(amount);
         int expected=200;
         assertEquals(expected, actual);
+      }
     }
-}
