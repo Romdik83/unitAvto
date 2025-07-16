8ef1598a3b8e3fcea9af33878f818946e38b1002
Romdik <romdik83@gmail.com>
2025-07-16 16:35:09 +0300

    Testng

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -18,4 +18,49 @@ class CashbackHackServiceTest {
         int expected=100;
         assertEquals(actual, expected);
     }
+
+    @Test
+    void whenBuyingAt800TheSystemOffersToBuyAnother200() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 800;
+        int actual= service.remain(amount);
+        int expected=200;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void whenBuyingFor1000TheSystemDoesNotOfferAnything() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual= service.remain(amount);
+        int expected=0;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void whenBuyingFor1100TheSystemDoesNotOfferAnything() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1100;
+        int actual= service.remain(amount);
+        int expected=0;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void whenBuyingAt1900TheSystemOffersToBuyAnother100() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1900;
+        int actual= service.remain(amount);
+        int expected=100;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void whenBuyingAt1800TheSystemOffersToBuyAnother200() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1800;
+        int actual= service.remain(amount);
+        int expected=200;
+        assertEquals(actual, expected);
+    }
 }
