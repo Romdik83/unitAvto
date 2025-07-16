ded12dbd5a565e64c01c8f923110411fbb898c3b
Romdik <romdik83@gmail.com>
2025-07-16 16:46:02 +0300

    Junit

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -17,6 +17,42 @@ public class CashbackHackServiceTest {
         int amount = 900;
         int actual= service.remain(amount);
         int expected=100;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void whenBuyingAt800TheSystemOffersToBuyAnother200() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 800;
+        int actual= service.remain(amount);
+        int expected=200;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void whenBuyingFor1000TheSystemDoesNotOfferAnything() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual= service.remain(amount);
+        int expected=0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void whenBuyingAt1900TheSystemOffersToBuyAnother100() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1900;
+        int actual= service.remain(amount);
+        int expected=100;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void whenBuyingAt1800TheSystemOffersToBuyAnother200() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1800;
+        int actual= service.remain(amount);
+        int expected=200;
+        assertEquals(expected, actual);
     }
 }
